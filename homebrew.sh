eb4f3627fdbf10bfac950ed1d5f43a83ceae4c50
Marcell Jusztin <marcell.jusztin@infinityworks.com>
2019-03-21 16:56:41 +0000

    add homebrew install scripts

---
+++ homebrew.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+declare -a brews=("ack"
+                  "fd"
+                  "fontconfig"
+                  "fzf"
+                  "git"
+                  "gnupg"
+                  "go"
+                  "graphviz"
+                  "imagemagick"
+                  "neovim"
+                  "node"
+                  "openssl"
+                  "python3"
+                  "ripgrep"
+                  "terraform"
+                  "terraform_landscape"
+                  "the_silver_searcher"
+                  "tmux"
+                  "yarn"
+                  "z"
+                  "zsh"
+                  "zsh-completions"
+                  )
+
+declare -a casks=("alacritty"
+                  "keybase"
+                  "firefox"
+                  "google-chrome"
+                  "slack"
+                  "docker"
+                  )
+
+# Install CommandLineTools
+if ! [ -d "/Library/Developer/CommandLineTools" ]; then
+    sudo xcode-select --install
+else
+    echo "Bootstrap.sh => CommandLineTools already installed."
+fi
+
+# Install Homebrew
+if ! hash brew 2>/dev/null; then
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew analytics off
+else
+    echo "Bootstrap.sh => Homebrew already installed."
+fi
+
+# Install brews
+for brew in "${brews[@]}"
+do
+   brew install "$brew"
+done
+
+# Install casks
+for cask in "${casks[@]}"
+do
+   brew cask install "$cask"
+done
+
+# Update, Upgrade and Clean Up
+brew update
+brew upgrade -all
+brew cleanup
